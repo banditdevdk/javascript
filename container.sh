4c9576c46642bca0dd10ab848a4373eb0d51b204
davidek-d <dajo1002@stud.ek.dk>
2026-09-24 10:55:56 +0200

    seventh

--- container.sh
+++ container.sh
@@ -6,3 +6,10 @@ docker exec hello-world ls /usr/share/nginx/html
 
 docker logs hello-world
 
+docker stop hello-world
+docker stop hello-world2
+
+docker ps -a
+
+docker rm hello-world
+docker rm hello-world2
